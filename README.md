8484ff32fd06e0936e2d1771dba7549054b9de4e
HP <mdzaid1299@gmail.com>
2022-11-12 15:08:38 +0530

    ini

---
+++ README.md
@@ -0,0 +1,16 @@
+2> Check given number is prime or not..
+
+ex: 19//true
+49//false
+
+step 1 : take input integer from user.
+
+step 2 : use for loop
+
+step 3 : prime no starts with 2 ,3,5,7,11,13.... 
+so based on that start loop from i= 2;
+
+step 4 : inside for loop give one condition by using if .. 
+think about logic
+
+step 5 : if condition is true then print whether it is prime or not
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("enter a integer");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            System.out.println("prime");
+        } else System.out.println("not prime");
     }
 }
